f58594bcc2adea62f35f33fb293f63f851319bb2
AluizioLucas <aluiziolucas@alu.ufc.br>
2022-04-17 10:44:03 -0300

    Atualização

--- Estressados/Estressados.cpp
+++ Estressados/Estressados.cpp
@@ -1,320 +1,452 @@
 #include <iostream>
 #include <vector>
-#include <cstdlib>
-#include <time.h>
+
 using namespace std;
-//existe: Alguém com o valor X está na fila?
-bool existe(int size, int vet[], int x){
-    for(int i = 0; i < size; i++){
-        if(vet[i] == x) return true;
+
+bool existe(vector<int> fila, int x){
+    for (int i = 0; i < fila.size(); i++){
+        if (fila[i] == x){
+            cout << "true" << endl;
+            return true;
+        }
     }
+    cout << "false" << endl;
     return false;
 }
 
-//contar: Quantas vezes o valor X apareceu na fila?
-int contar(int size, int vet[], int x){
-     int cont = 0;
-     for(int i = 0; i < size; i++){
-         if(vet[i] == x) cont++;
-    }
-    return cont;
-}
-
-//procurar: Em que posição da fila aparece X pela primeira vez?
-int procurar(int size, int vet[], int x){
-    for(int i = 0; i < size; i++){
-        if(vet[i] == x) {
-            return i;  
-        }
-        return -1;
-    }
-}
-
-//procurar_apartir: Dada a posição para iniciar a busca, qual a próxima posição em que aparece X?
-int procurar_apartir(int size, int vet[], int x, int inicio){
-    for(int i = inicio; i < size; i++){
-       if(vet[i] == x) {
-           return i;  
-        }
-        return -1;
-   }
-}
-
-//Melhor caso: 4 funções
-//procurar_menor: qual o menor valor da lista?
-int procurar_menor(int size, int vet[], int x){
-    int aux = vet[0];
-    for(int i = 0; i < size; i++){
-        if(vet[i] < aux) aux = vet[i];
-    }
-    return aux;
-}
-
-//procurar_pos_menor: qual a posição do menor valor da lista?
-int procurar_pos_menor(int size, int vet[], int x){
-    int aux = vet[0], pos{};
-    for(int i = 0; i < size; i++){
-       if(vet[i] < aux){
-           aux = vet[i];
-           pos = i;
+int contar(vector<int> fila, int x){
+    int count = 0;
+    for (int i = 0; i < fila.size(); i++){
+        if (fila[i] == x){
+            count += 1;
         }
     }
-   return pos;
+    cout << "Contou: "<< count<< endl; 
+    return count;
 }
 
-// //procurar_pos_menor_apartir: Dada a posição para iniciar a busca, qual a posição do menor valor da lista?
-int procurar_pos_menor_apartir(int size, int vet[], int x, int inicio){
-    int aux = vet[0], pos{};
-    for(int i = inicio; i < size; i++){
-        if(vet[i] > aux){
-           aux = vet[i];
-           pos = i;
-       }
-   }
-    return pos;
-}
+int procurar(vector<int> fila, int x){
+    int count = 0;
 
-//procurar_pos_melhor_se: qual a posição do HOMEM mais calmo? (menor valor maior que 0)
-int procurar_pos_melhor_se(int size, int vet[], int x){
-    int menorPositivo = vet[0], pos = 0, cont=0;
-    for(int i = 0; i < size; i++){
-        if(vet[i] > 0){
-            cont++;
-            if(vet[i]*-1 < menorPositivo) {
-                menorPositivo = vet[i];
-                pos = i;
-            }
+    for (int i = 0; i < fila.size(); i++){
+        if(fila[i] == x ){
+            cout <<"Posicao: " << i << endl;
+            return i;
         }
     }
-    if(cont == 0) {
-        return -1;
-    }
-    return pos;
+    cout << "Posicao: " << -1 << endl;
+    return -1;
+
 }
 
-//Contagem: 4 funções
-//calcular_stress_medio: qual a média de stress da fila? (abs)
-int calcular_stress_medio(int size, int vet[]){
-    int result = 0;
-    for(int i = 0; i < size; i++){
-        if(vet[i] > 0) result += vet[i];
-        else result += (vet[i] * -1);
-    }
-    return result / size;
-}
+int procurar_apartir(vector<int> fila, int x,int inicio){
+    int count = 0;
 
-//mais_homens_ou_mulheres: Na fila existem mais homens ou mulheres?
-string mais_homens_ou_mulheres(int size, int vet[]){
-    int h = 0, m = 0; 
-    for( int i = 0; i < size; i++){
-        if(vet[i] > 0) h++;
-        else m++;
-    }
-
-    if(h > m) return "homens";
-
-    else return "mulheres";
-
-    return "empate";
-}
-
-//qual_metade_eh_mais_estressada: O nível de stress somado de todas as pessoas da primeira metade é maior que o nível de stress somado das pessoas da segunda metade da fila? (abs)
-string qual_metade_eh_mais_estressada(int size, int vet[]){
-    int result1 = 0, result2 = 0;
-    for(int i = 0; i < size; i++){
-        if(vet[i] > 0){
-            if(i <= size/2)  result1 += vet[i];
-            else result2 += vet[i];
-        }
-        else{
-            if(i <= size/2) result1 += (vet[i] * -1);
-            else result2 += (vet[i] * -1);
+    for (int i = inicio; i < fila.size(); i++){
+        if(fila[i] == x ){
+            cout <<"Posicao(Apartir): " << i << endl;
+            return i;
         }
     }
-    if(result1 == result2) return "empate";
-    else if(result1 > result2) return "primeira";
-    return "Segunda";
+    cout << "Posicao(Apartir): " << -1 << endl;
+    return -1;
+
 }
 
-//BLOCO II
-//clonar: retorna um novo vetor copiando os valores do vetor de entrada
-vector<int> clonar(int size, vector<int> vet){
-    vector<int> v(size);
-    for (int i = 0; i < (vet.size()); i++) {
-        v[i] = vet[i];
-    }
-    return  v;
-}
+int procurar_menor(vector<int> fila){
+    int count = fila[0];
 
-//pegar_homens: retorne uma lista com os homens (valores positivos)
-vector<int> pegar_homens(int size, vector<int> vet){
-    vector<int> v(size)
-    int n = 0;
-    for(int i = 0; i < size; i++){
-        if(vet[i] > 0){
-            v[n] = vet[i];
-            n = n + 1;
+    for (int i = 0; i < fila.size(); i++){
+        if(fila[i] < count  ){
+            count = fila[i];
         }
     }
-    return  v;
+    cout << "Posicao(Menor): " << count << endl;
+    return count;
 
 }
 
-//pegar_calmos: retorne uma lista com as pessoas com stress menor que 10 (abs)
-vector<int> pegar_calmos(int size, vector<int> vet){
-    vector<int> v(size)
-    int n = 0;
-    for(int i=0 ; i < size ;i++){
-        if(vet[i] < 0){
-            if((vet[i] * (-1)) =< 10){
-                v[n] = vet[i];
-                n = n + 1;
-            }
-        }
-        else{
-            if(vet[i] =< 10){
-                v[n] = vet[i];
-                n = n + 1;
-            } 
-        }
-    } 
-
-    return  v;  
-}
-
-//pegar_mulheres_calmas: retorne uma lista com as mulheres com nível de stress menor que 10.(negativos menor que 10)
-vector<int> pegar_mulheres_calmas(int size, vector<int> vet){
-vector<int> v(size)
-    int n = 0;
-    for(int i=0 ; i < size ;i++){
-        if(vet[i] < 0){
-            if((vet[i] * (-1)) =< 10){
-                v[n] = vet[i];
-                n = n + 1;
-            }
+int procurar_pos_menor(vector<int> fila){
+    int count = fila[0];
+    int posicao = 0;
+    for (int i = 0; i < fila.size(); i++){
+        if(fila[i] < count  ){
+            count = fila[i];
+            posicao = i;
         }
     }
+    cout << "Posicao(Pos_Menor): " << posicao << endl;
+    return posicao;
+
 }
 
-//Acesso: 5 funções
-//inverter_com_copia
-vector<int> inverter_com_copia(int size, vector<int> vet){
-    vector<int> v(size);
-    a = size -1;
-    for (int i = 0; i < (vet.size()); i++) {
-        v[a] = vet[i];
-        a--;
+int procurar_pos_menor_apartir(vector<int> fila,int inicio){
+    int count = fila[0];
+    int posicao = 0;
+    for (int i = inicio; i < fila.size(); i++){
+        if(fila[i] < count  ){
+            count = fila[i];
+            posicao = i;
+        }
     }
-    return  v;
+    cout << "Posicao(Pos_Menor_Apartir): " << posicao << endl;
+    return posicao;
+
 }
 
-//inverter_inplace
-void inverter_inplace(int size, vector<int>vet){
-    int aux = 0;
-    int j = size;
-    for(int i = 0; i < (size); i++){
-        aux = vet[i];
-        vet[i] = vet[j];
-        vet[j] = aux;
-        j--;
-
-        if(j == i) break;
+int procurar_melhor_pos_se(vector<int> fila){
+    int count = 0;
+    int posicao = 0;
+    for(int i =0; i<fila.size();i++){
+        if(fila[i]>0){
+            count = fila[i];
+            break;
+        }else{
+            posicao = -1;
+        }
+    }
+   
+    if(posicao == -1){
+        cout << "Posicao(procurar_melhor_pos_se): " << posicao << endl;
+        return posicao;
+    }else{
+    for (int i = 0; i < fila.size(); i++){
+        if(fila[i] < count && fila[i] > 0 ){
+            count = fila[i];
+            posicao = i;
+        }
+    }
+    cout << "Posicao(procurar_melhor_pos_se): " << posicao << endl;
+    return posicao;
     }
 }
 
-//sortear
-int sortear(int size, vector<int>& v){
-    srand( (unsigned)time(NULL) );
-    return vet[rand() % size];
-}
-
-//embaralhar
-void embaralhar(vector<int>& v){
-    
-}
-
-//ordenar - selection sort utilizando
-void ordenar(vector<int>& v){
-}
-
-//Conjuntos: 3 funções
-//exclusivos: O fiscal permitiu a entrada de um único representante de cada valor, ou seja, uma lista dos valores que aparecem na fila sem repetição. Que conjunto foi formado?
-vector<int> exclusivos(int size, vector<int> vet)[
-    vector<int> v(size);
-    int m = 0;
-    for(int i = 0; i < size; i++){
-        int cont = 0;
-        for(int j = aux; j < size;j++){
-            if(vet[i] == v[j])cont++;
+float calcular_stress_medio(vector<int> fila){
+    int count = 0;
+    for (int i = 0; i < fila.size(); i++){
+        if(fila[i]<0){
+            fila[i] = fila[i]*-1;
         }
-        if(cont < 1){
-            v[m]=vet[i];
-            m++; 
-        }
-    }
-    return v;
-]
-
-//diferentes: Qual a lista dos diferentes níveis de stress que aparecem?
-vector<int> diferentes(int size vector<int> vet){
-    vector<int> v(size);
-    int aux = 0;
-    int m = 0;
-    for(int i = 0; i < size; i++){
-        int cont = 0;
-        for(int j = aux; j < size;j++){
-            if(vet[i] < 0){ 
-                aux = vet[i]*(-1);
-                if(aux == v[j])cont++;
-            }
-        }
-        if(cont < 1){
-            if(vet[i] < 0) v[m]=vet[i]*(-1);
-            else v[m]=vet[i];
-            m++; 
-        }
-    }
-    return v;
-}
-
-//abandonados: quais pessoas ficaram na fila após remover um exemplar de cada valor?
-vector<int> abandonados(int size, vector<int> vet){
-    vector<int> v(size);
-    int cont = 0;
-    for(int i = 1; i < size; i++){
-        for( int j= 0; j < size; j++){
-            if(vet[i] == vet[j]){ 
-                v[cont] = vet[i];
-                cont++;
-            }
-        }
-    }
-}
-//BLOCO III
-//sozinhos: Quais PESSOAS eram as únicas representantes do seu nível de stress na fila? (abs)
-
-
-int main(){
-    int size{}, x{};
-
-    scanf("%d", &size);
-    // int *vet2 = clonar(size, vet);
-    vector<int> vet1(size);
-    
-    for(int i = 0; i < size; i++) {
-        scanf("%d", &vet1[i]);
+        count += fila[i];    
     }
 
-    vector<int> vet2 = clonar(size, vet1);
-    cout << "Vetor copiado: ";
+    float media = count/fila.size();
 
-    for (size_t i = 0; i < vet2.size(); i++) {
-        cout << vet2.at(i) << " ";
+    cout << "Media de stress: " << media << endl;
+    return media;
+
+}
+
+string mais_homens_ou_mulheres(vector<int> fila){
+    int countH = 0;
+    int countM = 0;
+    for (int i = 0; i < fila.size(); i++){
+        if(fila[i]<0){
+            countM += 1;
+        }else{
+            countH+=1;
+        }   
+    }
+
+    if(countM>countH){
+         cout << "Mulheres" << endl;
+         return "Mulheres";
+    }else if(countH>countM){
+         cout << "Homens" << endl;
+         return "Homens";
+    }else{
+        cout << "Empate" << endl;
+         return "Empate";
+    }
+
+}
+
+string qual_metade_eh_mais_estressada(vector<int> fila){
+    int primeira = fila.size()/2;
+    int segunda = fila.size();
+    int countP = 0, countS = 0;
+    for (int i = 0; i < primeira; i++){
+        if(fila[i]<0){
+            fila[i] *= -1;
+            countP += fila[i];
+        }else{
+            countP+=fila[i];
+        }   
+    }
+    for (int i = primeira; i < segunda; i++){
+        if(fila[i]<0){
+            fila[i] *= -1;
+            countS += fila[i];
+        }else{
+            countS+=fila[i];
+        }   
+    }
+
+    if(countP>countS){
+         cout << "Primeira" << endl;
+         return "Primeira";
+    }else if(countS>countP){
+         cout << "Segunda" << endl;
+         return "Segunda";
+    }else{
+        cout << "Empate" << endl;
+         return "Empate";
+    }
+
+}
+
+string homens_sao_mais_estressados_que_mulheres(vector<int> fila){
+    int countM = 0;
+    int countH = 0;
+    for (int i = 0; i < fila.size(); i++){
+        if(fila[i]<0){
+            countM += fila[i];
+        }else{
+            countH+=fila[i];
+        }   
+    }
+    countM *= -1;
+    if(countM>countH){
+         cout << "Mulheres" << endl;
+         return "Mulheres";
+    }else if(countH>countM){
+         cout << "Homens" << endl;
+         return "Homens";
+    }else{
+        cout << "Empate" << endl;
+         return "Empate";
+    }
+
+}
+
+// BLOCO 2
+
+vector<int> clonar(const vector<int>& v){
+    std::vector<int> vetNovo;
+
+    for (int i = 0; i < v.size(); i++){
+        vetNovo.push_back(v[i]);
+    }
+    cout << "Vetor novo: ";
+    for(int i = 0; i<v.size();i++){
+        cout << vetNovo[i] << " ,";
     }
     cout << endl;
-    // printf("%d", contar(size, vet, 0));
-    // for(int i = 0; i < size; i++) {
-    //     printf("%d\n", &vet2[i]);
-    // }
-    return 0;
+    return vetNovo;
+}
+
+
+vector<int> pegar_homens(const vector<int>& v){
+    vector<int> vetNovo;
+    for (int i = 0; i < v.size(); i++){
+        if(v[i] > 0){
+            vetNovo.push_back(v[i]);
+        }
+    }
+    cout << "Homens: ";
+    for(int i = 0; i<vetNovo.size();i++){
+        cout << vetNovo[i] << " ,";
+    }
+    cout << endl;
+    return vetNovo;
+}
+
+vector<int> pegar_mulheres(const vector<int>& v){
+    vector<int> vetNovo;
+    for (int i = 0; i < v.size(); i++){
+        if(v[i] < 0){
+            vetNovo.push_back(v[i]);
+        }
+    }
+    cout << "Mulheres: ";
+    for(int i = 0; i<vetNovo.size();i++){
+        cout << vetNovo[i] << " ,";
+    }
+    cout << endl;
+    return vetNovo;
+}
+
+vector<int> pegar_calmos(const vector<int>& v){
+    vector<int> vetNovo;
+    for (int i = 0; i < v.size(); i++){
+        if( v[i] < 10 && v[i] > -10){
+             vetNovo.push_back(v[i]);
+        }
+    }
+    cout << "Calmos: ";
+    for(int i = 0; i<vetNovo.size();i++){
+        cout << vetNovo[i] << " ,";
+    }
+    cout << endl;
+    return vetNovo;
+}
+
+vector<int> pegar_mulheres_calmas(const vector<int>& v){
+    vector<int> vetNovo;
+    for (int i = 0; i < v.size(); i++){
+        if( v[i] < 0 && v[i] > -10){
+             vetNovo.push_back(v[i]);
+        }
+    }
+    cout << "Mulheres calmas: ";
+    for(int i = 0; i<vetNovo.size();i++){
+        cout << vetNovo[i] << " ,";
+    }
+    cout << endl;
+    return vetNovo;
+}
+
+vector<int> inverter_com_copia(const vector<int>& v){
+    vector<int> vetNovo;
+    for(int i = v.size()-1; i>=0;i--){
+        vetNovo.push_back(v[i]);
+    }
+    cout << "Vetor invertido: ";
+    for(int i = 0; i<vetNovo.size();i++){
+        cout << vetNovo[i] << " ,";
+    }
+    cout << endl;
+    return vetNovo;
+}
+/*
+void inverter_inplace(vector<int> v){
+     int aux = 0;
+     for(int i = v.size()-1;i>=v.size()/2;i--){
+        v[aux] = v[i];
+        aux+=1;
+     }
+     cout << "Vetor invertido sem vetor auxiliar: ";
+    for(int i = 0; i<v.size();i++){
+        cout << v[i] << " ,";
+    }
+    cout << endl;
+}
+*/
+
+int sortear(const vector<int>& v){
+    int size = v.size() - 1;
+    cout << "Sorteado: " << v[rand() % size] << endl;
+    return v[rand() % size];
+}
+
+/*
+void ordenar(vector<int> v){
+    int indice_menor = 0;
+    for (int i = 0; i < v.size(); i++){
+        
+            if(v[i] < v[indice_menor]){
+                indice_menor = i;
+            }
+            int aux = v[i];
+            v[i]= v[indice_menor];
+            v[indice_menor] = aux;
+        }
+        
+    cout << "Ordenado: ";
+    for(int i = 0; i<v.size();i++){
+        cout << v[i] << " ,";
+    }
+    cout << endl;
+}
+*/
+vector<int> exclusivos(const vector<int>& v){
+    vector<int> vetNovo;
+    vetNovo.push_back(v[0]);
+    for(int i = 0; i<v.size();i++){
+        for(int j = 0;j<v.size();j++){
+            if(vetNovo[i] != v[j]){
+            vetNovo[i] = v[j];
+            }
+        
+        }
+    }
+    
+
+    cout << "Exclusivos: " ;
+    for(int i = 0; i<v.size();i++){
+        cout << vetNovo[i];
+    }
+    
+    cout << endl;
+    return v;
+}
+/*
+vector<int> diferentes(const vector<int>& v){
+    vector<int> n;
+    int count = 0;
+    for(int i = 0; i<v.size();i++){
+        for(int j = v.size()-1; j>=0;j--){
+            if(v[i] == v[j]){
+                break;
+            }else{
+             count = 1;   
+            }
+        }
+        if(count = 1){
+            n[i] = v[i];
+        }
+    }
+}*/
+
+
+int main()
+{
+    existe({-1, -50, -99}, -1);
+    existe({-1, -50, -99}, 10);
+    
+
+    contar({-1, -50, -1, -99}, -1);
+    contar({-1, -50, -1, -99}, 10);
+
+    procurar({-1, -50, -1 -99}, -50);
+    procurar({-1, -50, -1, -99}, 10);
+
+    procurar_apartir({5, 3, -1, -50, -1, -99}, -1, 0);
+    procurar_apartir({5, 3, -1, -50, -1, -99}, -1, 3);
+    procurar_apartir({5, 3, -1, -50, -1, -99}, -1, 4);
+
+    procurar_menor({5, 3, -1, -50, -1, -99});
+
+    procurar_pos_menor({5, 3, -1, -50, -1, -99});
+
+    procurar_pos_menor_apartir({5, 3, -1, -50, -1, 10}, 3);
+
+    procurar_melhor_pos_se({5, 3, -1, -50, -1, -99});
+    procurar_melhor_pos_se({-1, -50, -1, -99});
+
+    calcular_stress_medio({-1, -50, -1, -99});
+
+    mais_homens_ou_mulheres({5, 3, -1, -50, -1, -99});
+    mais_homens_ou_mulheres({5, 3, 1, -50, -1, -99});
+    mais_homens_ou_mulheres({5, 3, 1, -50, -1, 99});
+
+    qual_metade_eh_mais_estressada({5, 3, -1, -50, -1, -99});
+    qual_metade_eh_mais_estressada({50, 98, 2, -50, -1, -99});
+    qual_metade_eh_mais_estressada({-51, 99, 1, -50, -1, -99});
+
+    homens_sao_mais_estressados_que_mulheres({1,2,3,-5,-6,-7});
+    homens_sao_mais_estressados_que_mulheres({-1,-2,-3,5,6,7});
+    homens_sao_mais_estressados_que_mulheres({-7,-6,-5,5,6,7});
+
+    //BLOCO 2
+
+    clonar({1,2,3,4});
+
+    pegar_homens({1,2,3,-5,-6,-7});
+
+    pegar_mulheres({1,2,3,-5,-6,-7});
+
+    pegar_calmos({1,2,3,-5,-6,-7});
+
+    pegar_mulheres_calmas({1,2,3,-5,-6,-7});
+
+    inverter_com_copia({1,2,3,-5,-6,-7});
+
+    //inverter_inplace({1,2,3,-5,-6,-7});
+
+    sortear({1,2,3,-5,-6,-7});
+
+   // ordenar({1,5,3,4,6,7,2});
+
+    exclusivos({1,5,3,4,6,7,2});
 }
